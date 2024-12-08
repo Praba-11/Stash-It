bbf7afa2fc46c1f11571bad7d76bec63c9757ddf
Prabakaran SK <skprabakaran@appranix.com>
2024-12-08 14:31:12 +0530

    Sample CRUD and Azure Storage Dependency configured.

---
+++ Sub-Cloud-App/src/main/java/example/mapper/Mapper.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) Appranix, Inc - All Rights Reserved.
+ *
+ * <p>Unauthorized copying of this file, via any medium is strictly prohibited.
+ *
+ * <p>Proprietary and confidential.
+ */
+package example.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+@org.apache.ibatis.annotations.Mapper
+public interface Mapper {
+
+    @Select("SELECT info FROM information WHERE roll = #{roll}")
+    String getInfo(Long roll);
+
+}
--- Sub-Cloud-App/src/main/java/example/service/Service.java
+++ Sub-Cloud-App/src/main/java/example/service/Service.java
@@ -1,9 +1,15 @@
 package example.service;
 
+import example.dao.DAO;
+import example.mapper.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @org.springframework.stereotype.Service
 public class Service {
 
-    public String getService() {
-        return "";
+    @Autowired private DAO dao;
+
+    public String getInfo() {
+        return dao.getInfo(1L);
     }
 }
