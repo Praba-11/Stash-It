83460f0a0f80281904d26bba45a3657aa0da9357
Prabakaran <102480416+Praba-11@users.noreply.github.com>
2025-07-12 12:59:05 +0530

    Delete Sub-Cloud-App directory

--- Sub-Cloud-App/src/main/resources/db/CREATE_ARTIFACT_TABLE.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE artifact (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    created_date DATE NOT NULL,
-    expiry_date DATE,
-    issued_by VARCHAR(255),
-    description TEXT
-);
